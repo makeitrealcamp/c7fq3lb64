ffcba2ebd492875abfdb6eec5e00e37fa43b7b87
pandai87 <pandai_87@hotmail.com>
2016-03-01 14:13:45 -0500

    Prueba de Querys

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,18 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+    @expenses = Expense.order("date DESC")
+
+    if params[:concept].present?
+    	@expenses = Expense.where("concept LIKE :concept", {concept:"%#{params[:concept]}%"})	 
+
+	elsif params[:category_id].present?
+    	@expenses = Expense.where("category_id = :category_id", {category_id:"#{params[:category_id]}%"})
+    else
+    	@expenses = Expense.where("concept LIKE :concept AND category_id = :category_id", {concept: "%#{params[:concept]}%",  category_id:"{params[:category_id]}"})	
+	
+    end 
+
   end
 end
+
