6119fbdcf525e9da71015a534122134b82af92b0
pandai87 <pandai_87@hotmail.com>
2016-03-01 14:29:50 -0500

    Se arregla con la solucion de Juan

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,16 +3,15 @@ class ExpensesController < ApplicationController
 
     @expenses = Expense.order("date DESC")
 
-    if params[:concept].present?
-    	@expenses = Expense.where("concept LIKE :concept", {concept:"%#{params[:concept]}%"})	 
+    if params[:category_id].present? && params[:concept].present?
+        @expenses = Expense.where("concept LIKE :concept AND category_id = :category_id", {concept: "%#{params[:concept]}%",  category_id:"#{params[:category_id]}"})
+      
 
-	elsif params[:category_id].present?
-    	@expenses = Expense.where("category_id = :category_id", {category_id:"#{params[:category_id]}%"})
-    else
-    	@expenses = Expense.where("concept LIKE :concept AND category_id = :category_id", {concept: "%#{params[:concept]}%",  category_id:"{params[:category_id]}"})	
-	
-    end 
+    elsif params[:concept].present?
+      @expenses = Expense.where("concept LIKE :concept", {concept:"%#{params[:concept]}%"})
 
+    elsif params[:category_id].present?
+      @expenses = Expense.where("category_id = :category_id", {category_id:"#{params[:category_id]}"})
+    end
   end
 end
-
