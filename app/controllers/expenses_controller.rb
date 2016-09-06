b36ec389cc1c9fbc32e145613ad122abda3c3e7d
hermesalexis <hermesagm@gmail.com>
2016-09-06 12:40:12 -0500

    QUery busqueda ruta index Expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,17 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+    unless params[:category_id].nil? && params[:concept].nil?
+      if params[:category_id].length == 0 &&  params[:concept].length == 0
+    	@expenses = Expense.order("date DESC")
+      elsif params[:category_id].length == 0
+    	@expenses = Expense.where("concept LIKE :concept", concept: "%#{params[:concept].upcase}%")
+      else
+    	@expenses = Expense.where("concept LIKE :concept",concept:"%#{params[:concept].upcase}%").
+                            where("category_id = :category_id", category_id: params[:category_id])
+      end
+    else
+      @expenses = Expense.order("date DESC")
+    end
   end
 end
