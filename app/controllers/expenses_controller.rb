87b042ea64a8660de49cd71e64069e6f2e2325b2
hermesalexis <hermesagm@gmail.com>
2016-09-07 17:18:03 -0500

    Query accion index expense por usuario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,12 +1,14 @@
 class ExpensesController < ApplicationController
 
-  def index
-    @expenses = Expense.order("date DESC")
+  def index 
+
+     @expenses = User.find(current_user).expenses
+
     if params[:concept].present?
       @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
     end
     if params[:category_id].present?
-      @expenses = @expenses.where("category_id = ?", params[:category_id])
+       @expenses = @expenses.where("category_id = ?", params[:category_id])
     end
   end
   
