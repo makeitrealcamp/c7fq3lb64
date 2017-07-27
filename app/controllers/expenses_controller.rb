df629148b53968daf336d6eae13770455abad5bf
Diego Camacho <hackvan@gmail.com>
2017-07-27 13:59:55 -0500

    Adiciona la funcionalidad de filtros en el index de expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,13 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    if params[:concept].present? && params[:category_id].present?
+      @expenses = Expense.where(["concept like ? AND category_id = ?", "%#{params[:concept]}%", params[:category_id]]).order("date DESC")
+    elsif params[:concept].present?
+      @expenses = Expense.where(["concept like ?", "%#{params[:concept]}%"]).order("date DESC")
+    elsif params[:category_id].present?
+      @expenses = Expense.where(["category_id = ?", params[:category_id]]).order("date DESC")
+    else
+      @expenses = Expense.order("date DESC")
+    end
   end
 end
