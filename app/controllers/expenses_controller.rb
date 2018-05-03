8a775b1cebc2594475bb46e380a1723d5196898e
Jhonnatan alexander Casas leon <JhonnatanCasasLeon@Jhonnatans-MacBook.local>
2018-05-02 20:41:34 -0500

    funcionalidad usuario con devise y filtro gastos propios

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,15 @@
 class ExpensesController < ApplicationController
+	before_action :authenticate_user!
   def index
-    @expenses = Expense.order("date DESC")
-    if params[:concept].present?
-      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    @user = User.find(current_user.id)
+
+    @expenses = Expense.where(user_id: current_user.id).order("date DESC")
+    
+    if params[:concept].present? 
+      @expenses = user.expenses.where("concept LIKE ?", "%#{params[:concept]}%")
     end
-    if params[:category_id].present?
-      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    if params[:category_id].present? 
+      @expenses = user.expenses.where("category_id = ?", params[:category_id])
     end
   end
 end
