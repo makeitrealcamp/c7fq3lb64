fb04baa99d98e24407c99d778d92e3218b9cacfe
Alejandro <acuellarh@gmail.com>
2019-07-30 17:44:34 -0500

    Authenticate User Before Action

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,7 @@
 class ExpensesController < ApplicationController
+  
+  before_action :authenticate_user!
+
   def index
     #  @expenses = Expense.order("date DESC")
     @expenses = Expense.order("date DESC")
